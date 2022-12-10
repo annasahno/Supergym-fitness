dd7dc144a92ef02c65a6f7b9d9909c7353f4455f
Anna <lilimp8@gmail.com>
2022-12-10 21:20:27 +0600

    добавляет раздел о тренажерном зале

---
+++ source/js/modules/video.js
@@ -0,0 +1,31 @@
+const videos = document.querySelectorAll('.video');
+
+const findVideos = () => {
+  videos.forEach((video) => setupVideo(video));
+};
+
+function setupVideo(video) {
+  let linkElement = video.querySelector('.video__link');
+  let buttonElement = video.querySelector('.video__button');
+  let link = linkElement.href;
+
+  linkElement.removeAttribute('href');
+
+  buttonElement.addEventListener('click', () => {
+    let iframe = createIframe(link);
+
+    linkElement.remove();
+    buttonElement.remove();
+    video.appendChild(iframe);
+  });
+}
+
+function createIframe(link) {
+  let iframe = document.createElement('iframe');
+  iframe.setAttribute('src', link + '?&autoplay=1');
+  iframe.classList.add('video__media');
+
+  return iframe;
+}
+
+export {findVideos};
