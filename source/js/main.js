1fc755547ce3a1e7bba7c8084d8d3442a5ecba86
Anna <lilimp8@gmail.com>
2022-12-13 21:12:25 +0600

    добавляет раздел тренеры

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {findVideos} from './modules/video.js';
 import {toggleTabs} from './modules/tabs.js';
+import './modules/slider-trainer.js';
 
 // ---------------------------------
 
